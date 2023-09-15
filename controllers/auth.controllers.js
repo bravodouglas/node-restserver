0ce21db58d557f5566aea84df3b902094587b7ff
dbravo <dbravo@movealong.tech>
2023-09-14 22:26:25 -0400

    google sing-in v2

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -47,47 +47,50 @@ const login = async (req, res = response) => {
 
 const googleSignIn = async (req, res = response) => {
   const { id_token } = req.body;
-
+    
   try {
-    //  const googleUser= await googleVerify(id_token)
-    //  console.log(googleUser);
-    const { correo, nombre, img } = await googleVerify(id_token);
+      const { correo, nombre, img } = await googleVerify( id_token );
 
-    let usuario = await Usuario.findOne({ correo });
+      let usuario = await Usuario.findOne({ correo });
 
-    if (!usuario) {
-      //Tengo que crearlo
-      const data = {
-        nombre,
-        correo,
-        password: "XX",
-        img,
-        google: true,
-      };
+      if ( !usuario ) {
+          // Tengo que crearlo
+          const data = {
+              nombre,
+              correo,
+              password: ':P',
+              img,
+              google: true
+          };
 
-      usuario = new Usuario(data);
-      await usuario.save();
-    }
+          usuario = new Usuario( data );
+          await usuario.save();
+      }
 
-    //Si el usuario en DB
-    if (!usuario.estado) {
-      return res.status(401).json({
-        msg: "Hable con el administrador, usuario bloqueado",
+      // Si el usuario en DB
+      if ( !usuario.estado ) {
+          return res.status(401).json({
+              msg: 'Hable con el administrador, usuario bloqueado'
+          });
+      }
+
+      // Generar el JWT
+      const token = await generarJWT( usuario.id );
+      
+      res.json({
+          usuario,
+          token
       });
-    }
-
-    //Generar el JWT
-    const token = await generarJWT(usuario.id);
-
-    res.json({
-      usuario,
-      token,
-    });
+      
   } catch (error) {
-    res.status(400).json({
-      msg: "Token de Google no es válido",
-    });
+
+      res.status(400).json({
+          msg: 'Token de Google no es válido'
+      })
+
   }
+
+
 };
 
 module.exports = {
